badff00c7fb63a18457054e438d5c7f6f37f3c67
Bino <furcraftlabs@gmail.com>
2025-10-17 18:28:31 +0700

    Update menuLoader.js

--- js/menuLoader.js
+++ js/menuLoader.js
@@ -1,8 +1,13 @@
 // /js/menuLoader.js
 async function loadMenu() {
     try {
-        const res = await fetch('/html/menu.html');
+        // Xác định folder gốc của Project Page
+        const projectRoot = "/Through-the-Gate/";
+
+        // Lấy menu.html từ folder /html/
+        const res = await fetch(projectRoot + "html/menu.html");
         const html = await res.text();
+
         document.getElementById('menu-placeholder').innerHTML = html;
     } catch (err) {
         console.error("Không thể tải menu:", err);
